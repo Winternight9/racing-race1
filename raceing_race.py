4bee1896eef6f3d4b2c6a446d4aab2623b38309d
Supakorn <supakorn@Supakorns-MacBook-Pro.local>
2019-03-22 14:00:30 +0700

    windowscreen

--- raceing_race.py
+++ raceing_race.py
@@ -1,2 +1,22 @@
 import arcade
 
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SCREEN_TITLE = 'Racing_Race'
+
+class MyGame(arcade.Window):
+    def __init__(self):
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+
+
+    def on_draw(self):
+        arcade.start_render()
+
+def main():
+    MyGame()
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()        
+ 
